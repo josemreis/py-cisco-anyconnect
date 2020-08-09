706868e922de0d90a6809d23f3261a1e87c89aa0
josemreis <josembcsreis@gmail.com>
2020-08-09 17:28:39 +0200

    sub-class init bug fixed

--- scripts/wrapper.py
+++ scripts/wrapper.py
@@ -10,8 +10,10 @@ from random import choice
 import os
 
 ### cisco_vpn class
-class cisco_vpn(username, pswrd_path, account_domain = "@uni-hamburg.de", host = "vpn.rrz.uni-hamburg.de", lastlog_path):
-    def __init__(self, username, pswrd_path):
+class cisco_vpn():
+    def __init__(self, username = None, pswrd_path = None, account_domain = "@uni-hamburg.de", host = "vpn.rrz.uni-hamburg.de", lastlog_path = None):
+        self.account_domain = account_domain
+        self.host = host
         self.lastlog_path = lastlog_path
         self.username_all = username
         self.pswrd = open(pswrd_path).read().rstrip("\n")
@@ -23,15 +25,23 @@ class cisco_vpn(username, pswrd_path, account_domain = "@uni-hamburg.de", host =
             ## if accounts > 1, retrieve last log and chose random account
             ## which is not the previous used one
             # get the next log account     
-            if len(self.username_all) > 1 and sequential_log and os.path.isfile(self.lastlog_path):
+            if len(self.username_all) > 1 and sequential_log:
+                if not os.path.isfile(self.lastlog_path):
+                    raise ValueError("path to lastlog text file missing!")
                 lastlog = open(self.lastlog_path).read().rstrip("\n")
                 next_log = choice([x for x in self.username_all if x != lastlog])
+                ## replace the last log in file
+                with open(self.lastlog_path) as f:
+                    lines = f.readlines()
+                lines[0] = next_log
+                with open(self.lastlog_path, "w") as f:
+                    f.writelines(lines)
             else:
                 next_log = self.username_all[0]
             # prepare the command for connecting
-            cmd = "printf '{account}\n{pswrd}\ny' | /opt/cisco/anyconnect/bin/vpn -s connect {host}".format(account = next_log + account_domain, 
+            cmd = "printf '{account}\n{pswrd}\ny' | /opt/cisco/anyconnect/bin/vpn -s connect {vpn_host}".format(account = next_log + self.account_domain, 
                                                                                                             pswrd = self.pswrd,
-                                                                                                            host = host)
+                                                                                                            vpn_host = self.host)
             ## run the command
             sub.call(cmd, shell = True)
         else:
@@ -39,7 +49,6 @@ class cisco_vpn(username, pswrd_path, account_domain = "@uni-hamburg.de", host =
     ## disconnect
     def cisco_disconnect():
         sub.call("/opt/cisco/anyconnect/bin/vpn disconnect", shell = True)
-        
     ## check the status
     def cisco_status():
         out = sub.check_output("/opt/cisco/anyconnect/bin/vpn state", shell = True)
