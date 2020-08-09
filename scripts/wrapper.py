54af989603510d603819eac9548d4aaf2427db1b
josemreis <josembcsreis@gmail.com>
2020-08-09 17:34:41 +0200

    sequential and random multilog options allowed

--- scripts/wrapper.py
+++ scripts/wrapper.py
@@ -11,21 +11,23 @@ import os
 
 ### cisco_vpn class
 class cisco_vpn():
-    def __init__(self, username = None, pswrd_path = None, account_domain = "@uni-hamburg.de", host = "vpn.rrz.uni-hamburg.de", lastlog_path = None):
+    def __init__(self, username = None, pswrd = None, account_domain = "@uni-hamburg.de", host = "vpn.rrz.uni-hamburg.de", lastlog_path = None):
         self.account_domain = account_domain
         self.host = host
         self.lastlog_path = lastlog_path
         self.username_all = username
-        self.pswrd = open(pswrd_path).read().rstrip("\n")
+        if isinstance(pswrd, str):
+            self.pswrd = pswrd
+        else:
+            raise ValueError("No password was provided.")
         self.is_eduroam = "eduroam" in str(sub.check_output("nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\: -f2", shell = True))
     ## log in 
-    def cisco_connect(self, sequential_log = True):
+    def cisco_connect(self, multilog_type = ["random", "sequential"]):
         ## check if connected to eduroam, if yes no need to connect to vpn
         if not self.is_eduroam:
-            ## if accounts > 1, retrieve last log and chose random account
-            ## which is not the previous used one
             # get the next log account     
-            if len(self.username_all) > 1 and sequential_log:
+            if len(self.username_all) > 1 and multilog_type == "sequential":
+                ## if sequential select a random account which is not equal to the last used
                 if not os.path.isfile(self.lastlog_path):
                     raise ValueError("path to lastlog text file missing!")
                 lastlog = open(self.lastlog_path).read().rstrip("\n")
@@ -36,6 +38,9 @@ class cisco_vpn():
                 lines[0] = next_log
                 with open(self.lastlog_path, "w") as f:
                     f.writelines(lines)
+            elif len(self.username_all) > 1 and multilog_type == "random":
+                ## just choose a random account
+                next_log = choice(self.username_all)
             else:
                 next_log = self.username_all[0]
             # prepare the command for connecting
