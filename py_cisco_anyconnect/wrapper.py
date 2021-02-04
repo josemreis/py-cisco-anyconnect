a16c8f993df4f3b06f6e328576ebffc0fbb4700a
José Reis <jmreis15@hotmail.com>
2021-02-04 23:30:10 +0000

    removed most class attributes

--- py_cisco_anyconnect/wrapper.py
+++ py_cisco_anyconnect/wrapper.py
@@ -8,32 +8,30 @@ Created on Sun Aug  9 15:20:59 2020
 import subprocess as sub
 
 ### cisco_vpn class
-class cisco_vpn():
-    def __init__(self, username: str, pswrd: str, account_domain = "my_domain.de", host = "vpn.xkz.my_domain.de", check_for_eduroam = True):
-        self.account_domain = account_domain
-        self.host = host
-        self.username = username
-        self.pswrd = pswrd
+class cisco_vpn:
+    def __init__(self, check_for_eduroam = True):
+        self.check_eduroam = check_for_eduroam
         if check_for_eduroam:
             self.is_eduroam = "eduroam" in str(sub.check_output("nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\: -f2", shell = True))
         else:
             self.is_eduroam = None
     ## log in 
-    def cisco_connect(self):
+    def cisco_connect(self, username = None, pswrd = None, account_domain = "my_domain.de", host = "vpn.xkz.my_domain.de"):
         ## check if connected to eduroam, if yes no need to connect to vpn
         if not self.is_eduroam:    
-            print(f'> Connecting user {self.username} to {self.account_domain}')
+            print(f'> Connecting user {username} to {account_domain}')
             # prepare the command for connecting
-            cmd = "printf '{account}\n{pswrd}\ny' | /opt/cisco/anyconnect/bin/vpn -s connect {vpn_host}".format(account = f'{self.username}@{self.account_domain}', 
-                                                                                                            pswrd = self.pswrd,
-                                                                                                            vpn_host = self.host)
+            cmd = "printf '{account}\n{pswrd}\ny' | /opt/cisco/anyconnect/bin/vpn -s connect {vpn_host}".format(account = f'{username}@{account_domain}', 
+                                                                                                            pswrd = pswrd,
+                                                                                                            vpn_host = host)
             ## run the command
             sub.call(cmd, shell = True)
         else:
-            print("currently connected to eduroam. Not connecting to the vpn.")
+            if self.check_eduroam:
+                print("currently connected to eduroam. Not connecting to the vpn.")
     ## disconnect
     def cisco_disconnect(self):
-        print(f'> Disconnecting user {self.username} from {self.account_domain}')
+        print(f'> Disconnecting from cisco anyconnect')
         sub.call("/opt/cisco/anyconnect/bin/vpn disconnect", shell = True)
     ## check the status
     def cisco_status(self):
